0c55d578c05d7648f8940a945abc271b3b34c5e1
Feyfa <fisikamodern00@gmail.com>
2024-08-04 07:23:48 +0700

    margin and overflow

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <NavbarComponent v-if="showNavbarSidebar()" />
   
     <div 
-      class="w-screen h-screen flex relative lg:static">
+      class="w-screen h-screen overflow-hidden flex relative lg:static">
       <SidebarComponent 
         class="sidebar h-screen overflow-y-auto absolute lg:static lg:w-[25%] xl:w-[20%] 2xl:[w-15%] lg:p-4 lg:border-r-2 lg:border-r-neutral-300 lg:bg-white lg:pt-[4.5rem] lg:shadow-neutral-500 lg:shadow-lg transition-all ease-in-out duration-300  flex flex-col gap-5 text-lg z-40 lg:z-0"
         :class="{'w-0 overflow-hidden p-0 pt-[4.5rem] border-none shadow-none': !this.$global.isSidebarOpen, ' w-[50%] sm500:w-[40%] sm:w-[30%] md:w-[25%] p-4 border-r-2 border-r-neutral-300 bg-white pt-[4.5rem] shadow-neutral-500 shadow-lg': this.$global.isSidebarOpen}"
