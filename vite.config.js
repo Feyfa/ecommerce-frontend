1f9e93bd74c724ed408b08b6d31de3545300d742
Feyfa <fisikamodern00@gmail.com>
2026-05-31 05:24:42 +0700

    chore: configure local ecommerce dev host

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { createLogger, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const appUrl = 'https://app.ecommerce.dev/'
+const logger = createLogger()
+const defaultInfo = logger.info
+const stripAnsi = (value) => value.replace(/\x1B\[[0-?]*[ -/]*[@-~]/g, '')
+
+logger.info = (message, options) => {
+  const plainMessage = stripAnsi(message)
+
+  if (plainMessage.includes('Local:') && plainMessage.includes('127.0.0.1:43010')) {
+    defaultInfo(`  -> Local:   ${appUrl}`, options)
+    return
+  }
+
+  defaultInfo(message, options)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  customLogger: logger,
   plugins: [
     vue(),
   ],
+  server: {
+    host: '127.0.0.1',
+    port: 43010,
+    strictPort: true,
+    hmr: {
+      protocol: 'wss',
+      host: 'app.ecommerce.dev',
+      clientPort: 443,
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
